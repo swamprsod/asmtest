e16063414893aed18079f01404821a0a14e5b03f
swamprsod <yatagarsux44@gmail.com>
2025-09-06 01:04:26 +0700

    hi

--- ejection.asm
+++ ejection.asm
@@ -1,1 +1,15 @@
-section _data
+section .data
+    odin db 12
+    dva db 13
+    tri db 14
+
+section .text
+    global _start
+
+_start:
+    push rax, [odin]
+    push rdi, [tri]
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
